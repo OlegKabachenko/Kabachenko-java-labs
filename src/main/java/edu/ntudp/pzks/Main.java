8e30b2303c3da5b68a2aa1e815cd676ff7417035
Kabachenko Oleg <olegkabachenko3@gmail.com>
2025-02-04 15:46:00 +0200

    Added lab0

---
+++ src/main/java/edu/ntudp/pzks/Main.java
@@ -0,0 +1,9 @@
+package edu.ntudp.pzks;
+
+import edu.ntudp.pzks.lr0.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld.printHello();
+    }
+}
---
+++ src/main/java/edu/ntudp/pzks/lr0/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.ntudp.pzks.lr0;
+
+public class HelloWorld {
+    public static void printHello() {
+        System.out.print("Hello world!");
+    }
+}
